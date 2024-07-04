6710e6f39b9ebc4b97e75bf615953c9dca9752e1
ADP999 <pdurga789@gmail.com>
2024-07-04 16:35:30 +0530

    Update ApisApplication.java

--- src/main/java/com/example/Apis/ApisApplication.java
+++ src/main/java/com/example/Apis/ApisApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApisApplication {
-
-	LoggerFactory loggerFactory=
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApisApplication.class, args);
